ae8206ff9a5df37e9688489a4b197d5bb222030d
yogeshwaray <yashaswi.yogeshwara@campsystems.com>
2021-10-23 22:46:24 +0530

    BasicIonicAppWithSolutionFIle

---
+++ Client/ClientApp/100DaysOfWorkout/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '100DaysOfWorkout',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
